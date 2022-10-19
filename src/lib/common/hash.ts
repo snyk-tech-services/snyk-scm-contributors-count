fa3190f7bf25c7e1f368647afdab76d51059a014
IlanTSnyk <ilan.torbaty@snyk.io>
2022-10-19 12:54:00 +0300

    feat: change hash function to use SHA256

--- src/lib/common/hash.ts
+++ src/lib/common/hash.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto';
 
 export function hashData(s: string): string {
-  const hashedData = crypto.createHash('sha1').update(s).digest('hex');
+  const hashedData = crypto.createHash('SHA256').update(s).digest('hex');
   return hashedData;
 }
--- test/lib/common/hash.test.ts
+++ test/lib/common/hash.test.ts
@@ -3,7 +3,7 @@ import * as utils from '../../../src/lib/common';
 describe('Testing utils functions', () => {
   test('Test hash function', () => {
     expect(utils.hashData('antoine@snyk.io')).toEqual(
-      '53427c999df1b032e80ab13da0df88bd071ab89f',
+      '0c372a7421f9b634f4e67385e8b720a914af82bd10dcb8ec01e42b8439b193aa',
     );
   });
 });
